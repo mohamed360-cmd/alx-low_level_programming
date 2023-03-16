cce5d9a6d7774b4cc94a7c3d90370b5bc456b4c0
Mohamed Abdiladif <mihabulle45a@gmail.com>
2023-03-16 03:58:45 -0700

    mod

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-int main(){
-	puts("\"Programming is like building a multilingual puzzle\n");
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
